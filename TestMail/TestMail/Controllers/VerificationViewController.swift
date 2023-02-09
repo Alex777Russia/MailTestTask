f126bac50c38248a1e5a4cbab1b26adc29664a2d
Alex777Russia <shevalex777@mail.ru>
2023-02-09 14:14:38 +0300

    MVC architecture created

---
+++ TestMail/TestMail/Controllers/VerificationViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TestMail
+//
+//  Created by Алексей Шевченко on 09.02.2023.
+//
+
+import UIKit
+
+class VerificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+        setDelegates()
+        setConstraints()
+    }
+
+    private func setupView() {
+        
+    }
+    
+    private func setDelegates() {
+        
+    }
+}
+
+extension VerificationViewController {
+    private func setConstraints() {
+        
+    }
+}
