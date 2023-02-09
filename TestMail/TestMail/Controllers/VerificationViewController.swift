1aec32f394127906e9f67ffcbd9a761beb3efbea
Alex777Russia <shevalex777@mail.ru>
2023-02-09 15:46:11 +0300

    correctly working textField

--- TestMail/TestMail/Controllers/VerificationViewController.swift
+++ TestMail/TestMail/Controllers/VerificationViewController.swift
@@ -23,6 +23,12 @@ class VerificationViewController: UIViewController {
     private let collectionView = MailCollectionView(frame: .zero,
                                                     collectionViewLayout: UICollectionViewFlowLayout())
     
+    private lazy var stackView = UIStackView(arrangedSubviews: [mailTextField,
+                                                               verificationButton,
+                                                               collectionView],
+                                        axis: .vertical,
+                                        spacing: 20)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,11 +40,21 @@ class VerificationViewController: UIViewController {
     private func setupView() {
         view.addSubview(backgroundImageView)
         view.addSubview(statusLabel)
+        view.addSubview(stackView)
+        verificationButton.addTarget(self,
+                                     action: #selector(verificationButtonTapped),
+                                     for: .touchUpInside)
     }
     
     private func setDelegates() {
         collectionView.dataSource = self
         collectionView.selectMailDelegate = self
+        mailTextField.textFieldDelegate = self
+    }
+    
+    @objc
+    private func verificationButtonTapped() {
+        print("ButtonTapped")
     }
 }
 
@@ -51,7 +67,7 @@ extension VerificationViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: idCell.idMailCell.rawValue,
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IdCell.idMailCell.rawValue,
                                                             for: indexPath) as? MailCollectionViewCell
         else {
             return UICollectionViewCell()
@@ -60,9 +76,23 @@ extension VerificationViewController: UICollectionViewDataSource {
     }
 }
 
+// MARK: - SelectProposedMailProtocol
+
 extension VerificationViewController: SelectProposedMailProtocol {
     func selectProposedMail(indexPath: IndexPath) {
         print(indexPath)
+    }
+}
+
+// MARK: - ActionsMailtextFieldProtocol
+
+extension VerificationViewController: ActionsMailtextFieldProtocol {
+    func typingText(text: String) {
+        print(text)
+    }
+    
+    func cleanOutTextField() {
+        print("clear")
     }
 }
 
@@ -77,6 +107,14 @@ extension VerificationViewController {
             statusLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
             statusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             statusLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            mailTextField.heightAnchor.constraint(equalToConstant: 50),
+            stackView.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 2),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
         ])
     }
 }
--- TestMail/TestMail/Views/VerificationVC/MainTextField.swift
+++ TestMail/TestMail/Views/VerificationVC/MainTextField.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 
+
+protocol ActionsMailtextFieldProtocol: AnyObject {
+    func typingText(text: String)
+    func cleanOutTextField()
+}
+
 class MailTextField: UITextField {
+    
+    weak var textFieldDelegate: ActionsMailtextFieldProtocol?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         configure()
+        delegate = self
     }
     
     required init?(coder: NSCoder) {
@@ -35,5 +44,22 @@ class MailTextField: UITextField {
         font = UIFont(name: "Apple SD Gothic Neo", size: 20)
         tintColor = #colorLiteral(red: 0.5019607843, green: 0.5019607843, blue: 0.5019607843, alpha: 1)
         translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension MailTextField: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.resignFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        textFieldDelegate?.typingText(text: text)
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        textFieldDelegate?.cleanOutTextField()
+        return true
     }
 }
