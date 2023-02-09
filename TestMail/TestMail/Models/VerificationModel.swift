974db81ec521090461c1c668079af45f9e60507a
Alex777Russia <shevalex777@mail.ru>
2023-02-09 16:40:15 +0300

    correctly logic validation of authorization finished

--- TestMail/TestMail/Models/VerificationModel.swift
+++ TestMail/TestMail/Models/VerificationModel.swift
@@ -31,7 +31,20 @@ class VerificationModel {
         })
     }
     
+    private func deriveNameMail(text: String) {
+        guard let atSimbolIndex = text.firstIndex(of: "@") else { return }
+        let endIndex = text.index(before: atSimbolIndex)
+        let firstIndex = text.startIndex
+        let range = text[firstIndex...endIndex]
+        
+        nameMail = String(range)
+    }
+    
     public func getFiltredMail(text: String) {
         filtringMails(text: text)
+    }
+    
+    public func getMailName(text: String) {
+        deriveNameMail(text: text)
     }
 }
